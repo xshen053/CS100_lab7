0fc82415777be2cd8f9cac2380487131a952e287
BFLion <hyu146@ucr.edu>
2022-03-03 03:58:41 -0800

    remove //

---
+++ unit_test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+#include "op_test.hpp"
+#include "Rand_test.hpp"
+#include "mult_test.hpp"
+#include "div_test.hpp"
+#include "addtest.hpp"
+#include "subtest.hpp"
+#include "pow_test.hpp"
+#include "visitor_test.hpp"
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    }
